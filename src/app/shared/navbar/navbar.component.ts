1bc0d7af922ca8df8b99592ed53cfde2d7c35a22
Dan Skelton <20068562+Ombudsdan@users.noreply.github.com>
2024-07-23 21:01:30 +0100

    Implemented a new responsive navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SocialLinksComponent } from '../../components/social-links/social-links.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faInstagram,
+  faThreads,
+  faXTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
-  imports: [RouterModule, FontAwesomeModule, SocialLinksComponent],
+  imports: [
+    RouterModule,
+    FontAwesomeModule,
+    SocialLinksComponent,
+    FontAwesomeModule,
+  ],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  faBars = faBars;
+  faInstagram = faInstagram;
+  faThreads = faThreads;
+  faFacebook = faFacebook;
+  faX = faXTwitter;
+
+  /* Toggle between adding and removing the "responsive" class to navmenu when the user clicks on the icon */
+  toggleResponsive() {
+    const navbarEl = document.getElementById('navmenu') ?? new HTMLElement();
+    navbarEl.className =
+      navbarEl.className === 'navmenu' ? 'navmenu responsive' : 'navmenu';
+  }
+}
