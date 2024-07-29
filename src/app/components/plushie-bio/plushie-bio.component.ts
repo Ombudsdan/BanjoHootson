5626b44382d31daa14f01afa684fa745d5c503c5
Dan Skelton <20068562+Ombudsdan@users.noreply.github.com>
2024-07-29 15:24:50 +0100

    Combined about-page components into the page

---
+++ src/app/components/plushie-bio/plushie-bio.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import Plushie from '../../shared/interfaces/plushie';
+
+@Component({
+  selector: 'app-plushie-bio',
+  standalone: true,
+  imports: [],
+  templateUrl: './plushie-bio.component.html',
+  styleUrl: './plushie-bio.component.scss',
+})
+export class PlushieBioComponent {
+  @Input({ required: true }) plushie!: Plushie;
+
+  name: Plushie['name'] = '';
+  bio: Plushie['bio'] = '';
+  birthday: Plushie['birthday'] = '';
+  imgFileName: Plushie['imgFileName'] = '';
+
+  ngOnChanges() {
+    this.name = this.plushie.name;
+    this.bio = this.plushie.bio;
+    this.birthday = this.plushie.birthday;
+    this.imgFileName = this.plushie.imgFileName;
+  }
+}
